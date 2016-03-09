1d90aaa05e4c8b322fa2d7705def2fe57fdc58d6
hb <295851252@qq.com>
2016-03-09 16:47:19 +0800

    update register_user.php

--- register_user.php
+++ register_user.php
@@ -5,47 +5,26 @@
 </head>
 <body>
 <?php
-    if(isset($_POST['name'])){
+    if(isset($_POST['submit']) && $_POST['submit']='注册'){
         $name = $_POST['name'];
-    }else{
-        echo "请填写用户名<br>";
-        echo"<script type='text/javascript'>alert('请填写用户名');location='register.html';  </script>";
-    }
-    if(isset($_POST['passwd'])){
         $pwd = $_POST['passwd'];
-    }else{
-        //echo "请填写密码<br><a href='login.php'>返回</a>";
-        echo"<script type='text/javascript'>alert('请填写密码');location='register.html';</script>";
-    }
-    if(isset($_POST['cpasswd'])){
         $cpwd = $_POST['cpasswd'];
-    }
-    if($pwd != $cpwd){
-        echo"<script type='text/javascript'>alert('密码不一致，请重新输入！');location='register.html';</script>";
-    }
-    if(isset($_POST['phone'])){
         $phone=$_POST['phone'];
-    }else{
-        echo"<script type='text/javascript'>alert('请填写电话号码');location='register.html';</script>";
-    }
-    if(isset($_POST['email'])){
         $email=$_POST['email'];
-    }
-    if(isset($_POST['school'])){
         $school=$_POST['school'];
-    }
-    if(isset($_POST['Grade'])){
         $grade=$_POST['Grade'];
-    }
-    if(isset($_POST['major'])){
         $major=$_POST['major'];
-    }
-    if(isset($_POST['bit_class'])){
         $bit_class=$_POST['bit_class'];
-    }
-    $userinfo = collet_data();
-    if($userinfo['phone'] = $phone){
-        echo"<script type='text/javascript'>alert('该手机已经注册！');location='register.html';</script>";
+
+        if( $name == "" || $pwd == "" || $cpwd == "" || $phone == "" ){
+            echo "<script>alert('有字段为空，请重新输入！'); history.go(-1);</script>";
+        }else{
+            if( $pwd == $cpwd ){
+                echo "insert done...<br/>"
+            }else{
+                echo "<script>alert('密码不一致！'); history.go(-1);</script>";
+            }
+        }
     }
 ?>
 </body>
