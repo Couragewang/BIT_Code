e9265d0d279f75aca00c4515d46f32cab6788457
root <root@bogon.DHCP>
2016-03-09 22:23:03 -0800

    add jump js

--- register_user.php
+++ register_user.php
@@ -5,27 +5,27 @@
 <?php
 	//phpinfo();
     require_once("mysql_db_api.php");
-    //if( isset($_POST['submit']) && $_POST['submit']=='注册'){
-       // $name = $_POST['name'];
-       // $pwd = $_POST['passwd'];
-       // $cpwd = $_POST['cpasswd'];
-       // $phone=$_POST['phone'];
-       // $email=$_POST['email'];
-       // $school=$_POST['school'];
-       // $grade=$_POST['grade'];
-       // $major = $_POST['major'];
-       // $bit_class=$_POST['bit_class'];
+    if( isset($_POST['submit']) && $_POST['submit']=='注册'){
+        $name = $_POST['name'];
+        $pwd = $_POST['passwd'];
+        $cpwd = $_POST['cpasswd'];
+        $phone=$_POST['phone'];
+        $email=$_POST['email'];
+        $school=$_POST['school'];
+        $grade=$_POST['grade'];
+        $major = $_POST['major'];
+        $bit_class=$_POST['bit_class'];
 
 		//debug
-		$name = "perter";
-		$pwd="123456";
-		$cpwd="123456";
-		$phone="123456789";
-        $email="qq";
-        $school="qinghua";
-        $grade="wuban";
-        $major = "jihke";
-        $bit_class="5";
+		//$name = "perter";
+		//$pwd="123456";
+		//$cpwd="123456";
+		//$phone="123456789";
+        //$email="qq";
+        //$school="qinghua";
+        //$grade="wuban";
+        //$major = "jihke";
+        //$bit_class="5";
         if( $name == "" || $pwd == "" || $cpwd == "" || $phone == "" ){
             echo "<script>alert('输入信息有空值！'); history.go(-1);</script>";
         }else{
@@ -46,7 +46,7 @@
                 echo "<script>alert('密码不一致！'); history.go(-1);</script>";
             }
         }
-	//}
+	}
 ?>
 	</head>
 	<body>
