a40033abe25115f187f8c30c0c5c72b34d21c3f9
root <root@bogon.DHCP>
2016-03-09 21:16:03 -0800

    register success

--- mysql_db_api.php
+++ mysql_db_api.php
@@ -1,26 +1,23 @@
-<!DOCTYPE html>
-<html>
-<head lang="en">
-    <meta charset="UTF-8">
-</head>
-<body>
 <?php
     //$data=name,pwd,...,bit_class
     function insert_data( $data ){
-        $con = mysql_connect("localhost","root","");
-        if(!$con){
-            die('mysql connect error: '.mysql_error());
+		$servername="localhost";
+		$username="student";
+		$passwd="123456";
+		$db="bit";
+        $sql="INSERT INTO student_base_info (name, passwd, phone, email, school, grade, major, bit_class) VALUES "."("."$data".")";
+		//echo ".......... $sql............";
+		$conn=mysqli_connect($servername, $username, $passwd, $db);
+        if(mysqli_connect_errno($conn)){
+			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			return false;
         }
-        //normal code
-        mysql_select_db("bit", $con);
-        $sql="INSERT INTO student_base_info (name, passwd, phone, email, school, grade, major, bit_class) VALUES "."(".$data.")";
-        echo $sql<br/>;
-        if (!mysql_query($sql,$con))
-        {
-            die('insert error: '.mysql_error());
+        if (!mysqli_query($conn, $sql)){
+			echo "query sql failed";
+			return false;
         }
-        mysql_close($con);
+		mysqli_close($conn);
+		return true;
     }
+	//insert_data("'1','1','1','1','1','1','1','1'");
 ?>
-</body>
-</html>
--- register_user.php
+++ register_user.php
@@ -5,9 +5,9 @@
 </head>
 <body>
 <?php
-    require("mysql_db_api.php");
-
-    if(isset($_POST['submit']) && $_POST['submit']='注册'){
+	//phpinfo();
+    require_once("mysql_db_api.php");
+    if( isset($_POST['submit']) && $_POST['submit']=='注册'){
         $name = $_POST['name'];
         $pwd = $_POST['passwd'];
         $cpwd = $_POST['cpasswd'];
@@ -15,20 +15,36 @@
         $email=$_POST['email'];
         $school=$_POST['school'];
         $grade=$_POST['grade'];
-        $major=$_POST['major'];
+        $major = $_POST['major'];
         $bit_class=$_POST['bit_class'];
 
+		//debug
+	//	$name = "perter";
+	//	$pwd="123456";
+	//	$cpwd="123456";
+	//	$phone="123456789";
+    //    $email="qq";
+    //    $school="qinghua";
+    //    $grade="wuban";
+    //    $major = "jihke";
+    //    $bit_class="5";
         if( $name == "" || $pwd == "" || $cpwd == "" || $phone == "" ){
             echo "<script>alert('输入信息有空值！'); history.go(-1);</script>";
         }else{
             if( $pwd == $cpwd ){
-			    $data = $name.",".$pwd.",".$phone.",".$email.",".$school.",".$grade.",".$major.",".$bit_class;
-			    insert_data( $data );
+	    	    $data = "'$name'".","."'$pwd'".","."'$phone'".","."'$email'".","."'$school'".","."'$grade'".","."'$major'".","."'$bit_class'";
+				//echo $data;
+	    	    $ret = insert_data( $data );
+	    		if($ret == true ){
+	    			echo "success<br/>";
+	    		}else{
+	    			echo "failed<br/>";
+	    		}
             }else{
                 echo "<script>alert('密码不一致！'); history.go(-1);</script>";
             }
         }
-    }
+	}
 ?>
 </body>
 </html>
