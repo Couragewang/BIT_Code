120440646d00be9e5a111b7c8393b4e3ac2cafb8
root <root@bogon.DHCP>
2016-03-09 22:46:41 -0800

    fix login_in php

--- login_in.php
+++ login_in.php
@@ -1,3 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
 <?php
 /**
  * Created by PhpStorm.
@@ -10,8 +14,13 @@ require_once("mysql_db_api.php");
 $name = $_POST["name"];
 $passwd = $_POST["passwd"];
 $info = get_name_passwd();
-if($name == $info["name"] && $passwd["passwd"]){
+if($name == $info["name"] && $passwd == $info["passwd"]){
     echo "success\n";
 }else{
     echo "login failed\n";
 }
+?>
+<body>
+<h1>login</h1>
+</body>
+</html>
